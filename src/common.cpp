27ac7c46886d07b0f2b309133033a44b5048de72
Codeant <127379751+Codeant-GitHub@users.noreply.github.com>
2025-06-07 08:51:08 +0800

    2025-06-07 08:50:54

---
+++ src/common.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "common.hpp"
+
+#include "common.hpp"
+
+namespace YimMenu
+{
+	std::atomic<bool> g_Running{true};
+	HINSTANCE g_DllInstance{nullptr};
+	HANDLE g_MainThread{INVALID_HANDLE_VALUE};
+	DWORD g_MainThreadId = -1;
+}
