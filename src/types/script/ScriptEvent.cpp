1724cf8363ff52914b0c75963ef8875e20f5cf5b
Codeant <127379751+Codeant-GitHub@users.noreply.github.com>
2025-06-24 15:47:26 +0800

    2025-06-24 15:47:03

---
+++ src/types/script/ScriptEvent.cpp
@@ -0,0 +1,12 @@
+//https://github.com/Codeant-GitHub
+
+#pragma once
+#include "common.hpp"
+
+#include "ScriptEvent.hpp"
+#include "game/gta/Natives.hpp"
+
+void SCRIPT_EVENT::SendImpl(int size)
+{
+	SCRIPT::_SEND_TU_SCRIPT_EVENT_NEW(1, this, size / sizeof(std::uint64_t), PlayerBits, EventIndex);
+}
